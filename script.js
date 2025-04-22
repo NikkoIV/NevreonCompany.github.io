f407d76ba35db2a6e6c6b30110056efe01a5c53c
NikkoIV <nikkogtaiv@gmail.com>
2025-04-22 14:09:15 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const burgerIcon = document.querySelector(".burger-icon");
-    const burgerMenu = document.querySelector(".burger-menu");
-
-    // Переключение класса 'active' при клике на иконку бургера
-    burgerIcon.addEventListener("click", function () {
-        burgerMenu.classList.toggle("active");
-    });
-});
